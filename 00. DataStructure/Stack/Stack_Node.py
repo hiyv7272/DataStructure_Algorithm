c2fc7bce8c216472f1f83875bfdecc851395a849
hiyv7272 <hiyv7272@gmail.com>
2020-08-13 04:14:25 +0900

    Stack_Node (python) - __repr__() 수정

--- 00. DataStructure/Stack/Stack_Node.py
+++ 00. DataStructure/Stack/Stack_Node.py
@@ -1,7 +1,7 @@
 # Stack with Node
 class Node(object):
-    def __init__(self, value=None, pointer=None):
-        self.value = value
+    def __init__(self, data=None, pointer=None):
+        self.data = data
         self.pointer = pointer
 
 
@@ -13,8 +13,8 @@ class Stack(object):
     def isEmpty(self):
         return not bool(self.head)
 
-    def push(self, item):
-        self.head = Node(item, self.head) # value = item, pointer = self.head
+    def push(self, data):
+        self.head = Node(data, self.head)  # data = data, pointer = self.head
         self.count += 1
 
     def pop(self):
@@ -22,25 +22,26 @@ class Stack(object):
             node = self.head
             self.head = node.pointer
             self.count -= 1
-            return node.value
+            return node.data
         else:
             print('Stack is Empty')
 
     def peek(self):
         if self.count > 0 and self.head:
-            return self.head.value
+            return self.head.data
         else:
             print('Stack is Empty')
 
     def size(self):
         return self.count
 
-    def _printList(self):
+    def __repr__(self):
         node = self.head
+        data_list = list()
         while node:
-            print(node.value, end=' ')
+            data_list.insert(0, node.data)
             node = node.pointer
-        print()
+        return repr(data_list)
 
 
 # 초기 test code
@@ -51,10 +52,8 @@ if __name__ == "__main__":
     for i in range(11):
         stack.push(i)
 
-    print('Stack:')
-    stack._printList()
+    print('Stack:', stack)
     print('Stack peek:', stack.peek())
     for _ in range(5):
         print('Stack pop:', stack.pop())
-    print('Stack:')
-    stack._printList()
+    print('Stack:', stack)
