cbe91e61b2f9e289187614d7fd882c41bf3c1c06
hiyv7272 <hiyv7272@gmail.com>
2020-08-14 23:19:10 +0900

    Deque_Collections (python)

--- 00. DataStructure/Deque_collections.py
+++ 00. DataStructure/Deque_collections.py
@@ -3,6 +3,8 @@ from collections import deque
 
 # 초기 test code
 if __name__ == "__name__":
+    # dq = deque(maxlen=4)  ## 길이 조절 가
+
     dq = deque([])
     print('Insert in Deque 0 to 10')
     for i in range(11):
